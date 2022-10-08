1d3ce6b5391b0ac3caf7ac0a71b0fec60a40992a
Egorov Dmitry <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-10-08 17:24:15 +0300

    Add files via upload

---
+++ ITMO.SoftEng2022.ASP.NET.MVC.Lab3Ex3/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebMVCR1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional } // п.1.5
+                //defaults: new { controller = "My", action = "Index", id = UrlParameter.Optional } // п.2.2
+                //defaults: new { controller = "My", action = "Start", id = UrlParameter.Optional } // п.2.2
+            );
+        }
+    }
+}
