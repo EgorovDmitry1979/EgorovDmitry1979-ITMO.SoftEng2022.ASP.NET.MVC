69f1e536e438415bebe5f7183534d7a1fb8a9c5b
Egorov Dmitry <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-10-08 12:36:38 +0300

    Add files via upload

---
+++ ITMO.SoftEng2022.ASP.NET.MVC.Lab1Ex2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVCR1.Controllers
+{
+    //public class HomeController : Controller 
+        public class MyController : Controller
+    {
+        // GET: HomeController
+        // п.5
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //п.5
+        //public string Index() //
+        //public string Start() //  п.1.7, п.2.2
+        public string Index(string hel) // п.2.2
+        {
+            int hour = DateTime.Now.Hour;
+            //string Greeting = hour < 12 ? "Доброе утро" : "Добрый день"; // п.2.2
+            string Greeting = hour < 12 ? "Доброе утро" : "Добрый день" + ", " + hel; // п.2.2
+            return Greeting;
+        }
+    }
+}
---
+++ ITMO.SoftEng2022.ASP.NET.MVC.Lab1Ex3/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVCR1.Models;
+
+namespace WebMVCR1.Controllers
+{
+    //public class HomeController : Controller 
+        public class MyController : Controller
+    {
+        // GET: HomeController
+        // п.5
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //п.5
+        //public string Index() //
+        //public string Start() //  п.1.7, п.2.2
+        public string Index(string hel) // п.2.2
+        {
+            //int hour = DateTime.Now.Hour; // 3.2
+            //string Greeting = hour < 12 ? "Доброе утро" : "Добрый день"; // п.2.2
+            //string Greeting = hour < 12 ? "Доброе утро" : "Добрый день" + ", " + hel; // п.3.2
+            string Greeting = ModelClass.ModelHello() + ", " + hel; // п.3.2
+            return Greeting;
+        }
+    }
+}
