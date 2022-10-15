e08e15f6a34d01f7d22da444f156181d52e85fda
Egorov Dmitry <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-10-15 21:16:12 +0300

    Add files via upload

---
+++ FinalTask/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalTask.Models
+{
+    public class Student
+    {
+        public virtual int StudentId { get; set; } // ID студента
+        public virtual string LastName { get; set; } // Имя
+        public virtual string FirstName { get; set; } // Фамилия
+        public virtual string Surname { get; set; } // Отчество
+        public virtual double MiddleBall { get; set; } // Средний балл
+    }
+}
---
+++ FinalTask/Models/StudentsDbAdd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FinalTask.Models
+{
+    public class StudentsDbAdd: DropCreateDatabaseIfModelChanges<StudentDBStud>
+    {
+        protected override void Seed(StudentDBStud context)
+        {
+            context.Students.Add(new Student {LastName = "Смирнов", FirstName = "Максим", Surname = "Валентинович", MiddleBall = 75});
+            base.Seed(context);
+        }
+    }
+}
